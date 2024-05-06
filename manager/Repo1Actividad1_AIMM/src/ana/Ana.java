93011d3a23c77e07a60295d14e19a7ba3230eb6d
ana <anamgzr@gmail.com>
2024-05-06 10:27:35 +0200

    manager crea proyecto

---
+++ manager/Repo1Actividad1_AIMM/src/ana/Ana.java
@@ -0,0 +1,20 @@
+package ana;
+
+public class Ana {
+
+		
+			
+			public static void main(String[] args) {
+				String nombre = "ana";
+				
+				if (nombre == "ana")
+					System.out.println("correcto");
+				else
+					System.out.println("incorrecto");
+			
+			
+
+
+	}
+
+}
